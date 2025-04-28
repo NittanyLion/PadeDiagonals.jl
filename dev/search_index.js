19f0de09fbe13ab7a9a38c7c691c525136ad7250
Documenter.jl <documenter@juliadocs.github.io>
2025-04-28 19:55:07 +0000

    build based on b51efa2

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#PadeDiagonals","page":"Home","title":"PadeDiagonals","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PadeDiagonals.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
